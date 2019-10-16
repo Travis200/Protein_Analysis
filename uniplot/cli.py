f7481fe27cf6ff56889fb7f13233962f9d0faa35
b9029429 <Glasspane12>
2019-10-16 11:47:44 +0100

    Initial commit

---
+++ uniplot/cli.py
@@ -0,0 +1,30 @@
+import argparse
+import gzip
+from Bio import SeqIO
+
+def dump(args):
+    handle = gzip.open("uniprot_receptor.xml.gz")
+    for record in SeqIO.parse(handle, "uniprot-xml"):
+        print(record)
+
+def names(args):
+    handle = gzip.open("uniprot_receptor.xml.gz")
+    for record in SeqIO.parse(handle, "uniprot-xml"):
+        print(record.name)
+
+def cli():
+    ## Create a new parser
+    parser = argparse.ArgumentParser(prog="uniplot")
+
+    subparsers = parser.add_subparsers(help="Sub Command Help")
+
+    ## Add subparsers
+    subparsers.add_parser("dump").set_defaults(func=dump)
+    subparsers.add_parser("list").set_defaults(func=names)
+
+    ## Parse the command line
+    args = parser.parse_args()
+
+    ## Takes the func argument, which points to the function to call it
+    args.func(args)
+
