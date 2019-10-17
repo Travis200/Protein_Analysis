96f525e2a30180c3104761a79b3ef246dc5ba58e
b9029429 <Glasspane12>
2019-10-17 11:29:44 +0100

    working average

---
+++ uniplot/analysis.py
@@ -0,0 +1,10 @@
+def average_len(records):
+    count = 0
+    total_Length = 0
+    for i in records:
+        count = count + 1
+        total_Length = total_Length + len(i.name)
+        average = total_Length/count
+    return average
+
+
