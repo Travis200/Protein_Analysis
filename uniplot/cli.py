7aaed174cabdbdf56770c1aaa970c601426680ef
b9029429 <Glasspane12>
2019-10-24 11:22:00 +0100

    working with bar chart markup argument for changing uniprot file

--- uniplot/cli.py
+++ uniplot/cli.py
@@ -3,27 +3,27 @@ from . import parse
 from . import analysis
 from . import plot
 
-LOC="uniprot_receptor.xml.gz"
-
 def dump(args):
-    for record in parse. uniprot_seqrecords(LOC):
+    for record in parse. uniprot_seqrecords(args.LOC):
         print(record)
 
 def names(args):
-    for record in parse. uniprot_seqrecords(LOC):
+    for record in parse. uniprot_seqrecords(args.LOC):
         print(record.name)
 
 def average(args):
     print("Average Length is {}".format(
-        analysis.average_len(parse.uniprot_seqrecords(LOC))))
+        analysis.average_len(parse.uniprot_seqrecords(args.LOC))))
 
 def plot_average_by_taxa(args):
-    av = analysis.average_len_taxa(parse.uniprot_seqrecords(LOC))
+    av = analysis.average_len_taxa(parse.uniprot_seqrecords(args.LOC))
     plot.plot_bar_show(av)
 
 def cli():
     ## Create a new parser
     parser = argparse.ArgumentParser(prog="uniplot")
+    ## This is the optional arguements
+    parser.add_argument("-file",dest="LOC", default="uniprot_receptor.xml.gz")
 
     subparsers = parser.add_subparsers(help="Sub Command Help")
 
@@ -32,7 +32,6 @@ def cli():
     subparsers.add_parser("list").set_defaults(func=names)
     subparsers.add_parser("average").set_defaults(func=average)
     subparsers.add_parser("plot_average_by_taxa").set_defaults(func=plot_average_by_taxa)
-
     ## Parse the command line
     args = parser.parse_args()
 
